f00a25b0d832b906f5723ad38c40a1ba9b5decc9
clinicaespecializada <administrativo@clinicaespecializada.com.br>
2024-09-05 14:06:24 -0300

    feat: migraçao

--- patients/models.py
+++ patients/models.py
@@ -1,4 +1,7 @@
 from django.db import models
 
 class Patient(models.Model):
-    ...
+    name = models.CharField(max_length=155)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=155)
+    
