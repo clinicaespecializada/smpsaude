63e08455e23d355f04387cd40bf31c5a7de0ca03
clinicaespecializada <administrativo@clinicaespecializada.com.br>
2024-09-07 18:07:02 -0300

    feat: migrations

--- patients/models.py
+++ patients/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
 class Patient(models.Model):
-    name = models.CharField(max_length=155)
-    age = models.IntegerField()
-    gender = models.CharField(max_length=155)
-    
+    name = models.CharField(max_length=155, verbose_name='nome')
+    birth_date = models.IntegerField(verbose_name='data de nascimento')
+    gender = models.CharField(max_length=155, verbose_name='genero')
+    mother_name = models.CharField(max_length=155, verbose_name='Nome da mae')
+    father_name = models.CharField(max_length=155, verbose_name='Nome do pai', blank=True, null=True)
+    cpf = models.CharField(max_length=155)
+
+
+    def __str__(self) -> str:
+        return self.name
